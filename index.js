12c59dae4cb70c99efc89b99d815fb03287aa96e
federicoghedinastailamedia <federico.ghedina@stailamedia.com>
2016-11-11 13:15:42 +0100

    hei

---
+++ index.js
@@ -0,0 +1,37 @@
+require('malta').checkExec('babel');
+
+var path = require('path'),
+	fs = require('fs'),
+	child_process = require('child_process');
+
+function malta_es6(o, options) {
+
+	var self = this,
+		start = new Date(),
+		msg,
+		outname = o.name;
+	
+	options = options || {};
+
+	return function (solve, reject){
+		var ls = child_process.spawn('babel', [o.name, '--out-file', outname, '--presets', ['es2015']]);
+		
+		ls.on('exit', function (code) {
+			o.content = fs.readFileSync(o.name) + "";
+			msg = 'plugin ' + path.basename(__filename) + ' wrote ' + o.name;
+			solve(o);
+			self.notifyAndUnlock(start, msg);
+		});
+
+		ls.stdout.on('data', function(data) {
+			self.log_debug(data + "");
+		});
+
+		ls.stderr.on('error', function (data) {
+			self.log_err('stderr: ' + data);
+		});
+
+	};
+}
+malta_es6.ext = 'js';
+module.exports = malta_es6;
